209388283f9e9f40ad68e0c244fd8fea740b4904
ss77995ss <ss77995ss@gmail.com>
2022-12-20 09:19:56 -0500

    test(Breadcrumb): Fix <App /> test since we use `useMatches` for <Breadcrumb /> now

--- frontend/src/components/Breadcrumbs.js
+++ frontend/src/components/Breadcrumbs.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useMatches } from 'react-router-dom';
 
 export default function Breadcrumbs() {
@@ -8,15 +9,15 @@ export default function Breadcrumbs() {
     // now map them into an array of elements, passing the loader
     // data to each one
     .map((match) => match.handle.crumb(match.data));
-  console.log(matches);
+
   return (
     <div>
       <span></span>
       {crumbs.map((crumb, index) => (
-        <>
-          <span key={`breadcrumb-${index}`}>{crumb}</span>
+        <React.Fragment key={`breadcrumb-${index}`}>
+          <span>{crumb}</span>
           {index !== crumbs.length - 1 && <span> / </span>}
-        </>
+        </React.Fragment>
       ))}
     </div>
   );
